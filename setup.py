d3b59900d7951c77e08727adf1b4f9e6472db03f
Eduardo Leao <eduardoleao@Eduardos-MacBook-Air.local>
2023-12-28 17:46:28 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 ﻿from setuptools import setup, find_packages
 
-def read_file(path):
+def read_requirements(path):
   with open(path, 'r', encoding='utf-8-sig') as file:
+    data = file.read()
+  return data
+
+def read_readme(path):
+  with open(path, 'r') as file:
     data = file.read()
   return data
 
@@ -13,11 +18,11 @@ setup(
   maintainer = 'Eduardo Leitao da Cunha Opice Leao',
   url = 'https://github.com/eduardoleao052/Autograd-from-scratch',
   python_requires = '>=3.0',
-  install_requires = read_file('requirements.txt').split('\n'),
+  install_requires = read_requirements('requirements.txt').split('\n'),
   description = 'An educational framework similar to PyTorch, built to be interpretable and easy to implement.',
   license = 'MIT',
   keywords = 'autograd deep-learning machine-learning ai numpy python',
   packages = find_packages(),
-  long_description = read_file('README.md'),
+  long_description = read_readme('README.md'),
   long_description_content_type='text/markdown'
 )
